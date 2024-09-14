55f3cdd00a2343407b2404286909f3c83c95ac95
Arjan <arjan_randhawa@outlook.com>
2024-09-14 17:19:24 +0100

    Add - Allow Stores To Have Custom Refresh Intervals (v1.1.0)
    Version 1.1.0
    
    Feat:
    * Allow each store to have a custom refresh interval
    * Bump all packages to latest supported version
    * Bump Node.js required version to v21.17.0
    
    Remove:
    * Removed fastFech and slowFetch functionality, as this has been replaced by custom refresh intervals
      * Requires changes to config.json. Please check example.config.json to update your configuration
    * Removed experimental node flag for importing json files
      * Requires changes to config.json. Configuration file is now located in the root directory of the project

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 export const isJSON = (string: string): boolean => {
     let isJSON = true;
     try {
@@ -6,4 +8,9 @@ export const isJSON = (string: string): boolean => {
         isJSON = false;
     }
     return isJSON;
+};
+
+export const loadJSON = async (path: string) => {
+    const fileUrl = new URL(path, import.meta.url);
+    return JSON.parse(await fs.promises.readFile(fileUrl, 'utf8'));
 };
